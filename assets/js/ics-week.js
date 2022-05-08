1952084b52806b910d47fe4ba5d085b0ab19ebe0
Aaron Lam <aaronlam1004@gmail.com>
2022-05-07 22:01:04 -0700

    Adjusted some styling and added some blurbs/locations

--- assets/js/ics-week.js
+++ assets/js/ics-week.js
@@ -11,8 +11,7 @@ const EVENTS = {
       name: "WICS Crowdstrike Roundtable",
       time: "6:30 PM - 8 PM",
       date: 0,
-      location: "DBH 6011",
-      rsvp: "https://bit.ly/WICSxCrowdstrike",
+      location: "<a href='https://goo.gl/maps/5smLR77dkm8EM42j8' target='_blank'>DBH 6011</a>",
       zoom: "",
       blurb: "Ever wonder what it's like to work for any industry leader in cybersecurity? Join us Monday 5/9! 6:30 - 8 PM in DBH 6011.",
       poster: "assets/img/icsweek/crowdstrike.png"
@@ -23,7 +22,7 @@ const EVENTS = {
       name: "ICS Professor Q&A",
       time: "6 PM - 7 PM",
       date: 1,
-      location: "DBH 6011",
+      location: "<a href='https://goo.gl/maps/5smLR77dkm8EM42j8' target='_blank'>DBH 6011</a>",
       rsvp: "",
       zoom: "",
       blurb: "",
@@ -31,9 +30,9 @@ const EVENTS = {
     },
     {
       name: "ICSSC Typing Competition",
-      time: "6 PM - 7 PM",
+      time: "7:30 PM - 8:30 PM",
       date: 2,
-      location: "DBH 6011",
+      location: "<a href='https://goo.gl/maps/5smLR77dkm8EM42j8' target='_blank'>DBH 6011</a>",
       rsvp: "https://bit.ly/TypingTournament22",
       zoom: "",
       blurb: "Think you have the fastest fingers in ICS? Crown yourself the WPM winner of our typeracer tournament happening on Wednesday 5/11 from 7-9 PM at DBH 6011!",
@@ -41,13 +40,13 @@ const EVENTS = {
     },
     {
       name: "ICS Prom",
-      time: "6 PM - 9 PM",
+      time: "6:30 PM - 9 PM",
       date: 4,
-      location: "DBH 6011",
+      location: "<a href='https://goo.gl/maps/5smLR77dkm8EM42j8' target='_blank'>DBH 6011</a>",
       rsvp: "https://bit.ly/ICSProm",
       zoom: "",
       blurb: "Want to meet new people and make new relationships (platonically and romantically 👀)? Did COVID ruin your chances at your in-person Prom/Dances? If your answer is yes to either of these questions, come out and participate in our ICS Prom during ICS week on Friday, May 13th at 6:30PM. We will have fun activities including : dancing, eating, and socializing! As this is like many other proms, you should dress to impress - just like you’re impressing a date. You can still come without dressing up, but know that your fictional date will be disappointed. Hope to see everyone there!",
-      poster: ""
+      poster: "assets/img/icsweek/icsprom.png"
     }
   ],
   Data: [
@@ -55,10 +54,10 @@ const EVENTS = {
       name: "Data@UCI Python for Data Analysis Workshop",
       time: "7 PM - 8 PM",
       date: 1,
-      location: "DBH 6011",
-      rsvp: "",
+      location: "<a href='https://goo.gl/maps/qUkezxpfewj2G3Du5' target='_blank'>ISEB 1200</a>",
+      rsvp: "https://docs.google.com/forms/d/e/1FAIpQLSdrUkPs-PtE2gIs1WQm5xxIpEvrY0LxOQzK-EY1jUVIz252cw/viewform",
       zoom: "",
-      blurb: "",
+      blurb: "Want to learn how to analyze data with Python or add a new skill to your toolbox? Data @ UCI will be hosting an Intro to Python for Data Analysis Workshop in the Interdisciplinary Science and Engineering Building (ISEB) in Room 1200 on Tuesday, 5/10/2022 from 7-8:30 pm. This workshop is intended for people who have some experience with Python but are new to its applications in data analysis. We will be going over dataframes using the Pandas library. There will be free food and drinks for attendees as well as a gift card raffle! This workshop will be highly interactive so make sure to bring your laptop!",
       poster: ""
     }
   ],
@@ -91,10 +90,10 @@ const EVENTS = {
       name: "Hack@UCI Scratch-A-Thon",
       time: "5 PM - 6 PM",
       date: 3,
-      location: "TBD",
+      location: "<a href='https://goo.gl/maps/LRJaQVRiQyPomdA87' target='_blank'>ICS 428</a>",
       rsvp: "",
       zoom: "",
-      blurb: "",
+      blurb: "Hack@UCI is hosting our inaugural scratch-a-thon!! Come out to work on your very own Scratch hack and see what others have built 😼",
       poster: ""
     }
   ],
@@ -103,7 +102,7 @@ const EVENTS = {
       name: "VGDC Game Night",
       time: "8 PM - 10 PM",
       date: 3,
-      location: "DBH 1412",
+      location: "<a href='https://goo.gl/maps/5smLR77dkm8EM42j8' target='_blank'>DBH 1412</a>",
       rsvp: "",
       zoom: "",
       blurb: "",
@@ -115,10 +114,10 @@ const EVENTS = {
       name: "Design at UCI Picnic in Aldrich Park",
       time: "1 PM - 3 PM",
       date: 4,
-      location: "Aldrich Park",
+      location: "<a href='https://goo.gl/maps/4xXd1ddVCyhKEdNi7' target='_blank'>Aldrich Park</a>",
       rsvp: "",
       zoom: "",
-      blurb: "",
+      blurb: "Join us for a picnic in the middle of Aldrich where we'll host games, icebreakers, and food!",
       poster: ""
     }
   ],
@@ -142,7 +141,7 @@ function ShowEventInfo(club, eventIdx) {
   document.getElementById("event-title").innerText = eventInfo.name;
   document.getElementById("event-date").innerHTML = `<strong>${DATES[eventInfo.date]}</strong>, ${MONTH} ${START_DAY + eventInfo.date}, ${YEAR}`;
   document.getElementById("event-time").innerHTML = eventInfo.time;
-  document.getElementById("event-location").innerText = eventInfo.location;
+  document.getElementById("event-location").innerHTML = eventInfo.location;
 
   let rsvpLink = document.getElementById("rsvp-link");
   if (eventInfo.rsvp !== "") {
