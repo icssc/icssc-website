9846fd662e9afd406b64344c6099c810a4e45a16
Avent Chiu <65209258+sirAvent@users.noreply.github.com>
2022-07-21 14:16:00 -0700

    Migrate committees page to Next.js (#29)

--- pages/committees.js
+++ pages/committees.js
@@ -1,3 +1,24 @@
+import CommitteesJSON from '../assets/data/committees.json'
+import {Container } from 'react-bootstrap';
+
+
+function Committee(props) {
+  const {name, desc, members} = props;
+  return (
+    <Container className="justify-content-md-center text-center">
+      <h2 className="h1">{name}</h2>
+      <p className="h5 mb-0">{desc}</p>
+      <br/>
+      <Container>
+        <h3>{name} Members</h3>
+        <div>
+          {members.map(member => <p key={member}>{member}</p>)}
+        </div>
+      </Container>
+    </Container>
+  )
+}
+
 export default function Committees() {
   return (
     <>
@@ -8,238 +29,7 @@ export default function Committees() {
             <p class="h5 mb-0">ICS Student Council has five committees to provide different ways for students to contribute to the community. </p>
           </div>
         </section>
-
-        <section class="text-center bg-light u-content-space">
-          <div class="container">
-            <header class="text-center w-md-50 mx-auto mb-8">
-              <h2 class="h1">Public Relations Committee</h2>
-            </header>
-
-            <p class="h5 mb-0">
-              The Public Relations committee is in charge of keeping the ICS community informed on what's going on with ICS Student Council.
-              We keep our social medias (Instagram and Facebook) up to date with the latest news on events, workshops, and socials.
-              Since ICS Student Council works as an umbrella organizations for all the other wonderful ICS organizations/clubs on campus, we strive to help out the best we can in promoting all ICS events.
-              So if you are apart of an ICS organization and would like help in publicizing your events, please reach out to us. Anyone can feel free to contact us on our social media because we love to chat with you and answer any questions you may have about our organization or to just let you know what's going on!
-              Don't forget to like our Facebook page at <a href="https://facebook.com/ICSStudentCouncil/">https://facebook.com/ICSStudentCouncil/</a>" and follow us on Instagram at <a href="https://www.instagram.com/icssc.uci/">@icssc.uci</a>!
-            </p>
-
-
-            <br></br>
-            <div class="container">
-              <h3>Public Relations Committee Members</h3>
-              <div class="row justify-content-center">
-
-                <div class="col-lg-3 mb-5 mb-lg-0">
-                  <p>Nathan Nguyen</p>
-                  <p>Taylor Quach</p>
-                </div>
-
-                <div class="col-lg-3 mb-5 mb-lg-0">
-                  <p>Noah MacLean</p>
-                </div>
-
-                <div class="col-lg-3 mb-5 mb-lg-0">
-                  <p>Maithy Le</p>
-                </div>
-                <div class="col-lg-3 mb-5 mb-lg-0">
-                  <p>Priscilla Widjaja</p>
-                </div>
-
-              </div>
-
-            </div>
-          </div>
-        </section>
-
-        <section class="text-center u-content-space">
-          <div class="container">
-            <header class="text-center w-md-50 mx-auto mb-8">
-              <h2 class="h1">Events Committee</h2>
-            </header>
-
-            <p class="h5 mb-0">
-              The Events Committee coordinates and hosts a variety of different events designed for the ICS student body.
-              From encouraging new and old ICS students socially through things like the ICS Bonfire or our longer event Nerd Fighter, to improving skills technically and professionally like with our Med App Jam competition.
-              The Events Committee is here to help the students of ICS however they need.
-            </p>
-
-
-            <br></br>
-            <div class="container">
-              <h3>Events Committee Members</h3>
-              <div class="row justify-content-center">
-                <div class="col-lg-3 mb-5 mb-lg-0">
-                  <p>Andrea AhSue</p>
-                  <p>Calvin Nguyen</p>
-                  <p>Justin Yue</p>
-                  <p>Patrick Wang</p>
-                  <p>Wentao Chen</p>
-                </div>
-
-                <div class="col-lg-3 mb-5 mb-lg-0">
-                  <p>Angela Qiao</p>
-                  <p>Donny Pham</p>
-                  <p>Kaitlyn Suzuki</p>
-                  <p>Rebecca Ahn</p>
-                  <p>William Hou</p>
-                </div>
-
-                <div class="col-lg-3 mb-5 mb-lg-0">
-                  <p>Anna Chan</p>
-                  <p>Eric Liang</p>
-                  <p>Kevin Jung</p>
-                  <p>Rithvij Pochampally</p>
-                </div>
-
-                <div class="col-lg-3 mb-5 mb-lg-0">
-                  <p>Brian Chu</p>
-                  <p>Ivan Chow</p>
-                  <p>Nicole Nguyen</p>
-                  <p>Ryan Mohta</p>
-                </div>
-
-
-              </div>
-            </div>
-
-          </div>
-        </section>
-
-        <section class="text-center bg-light u-content-space">
-          <div class="container">
-            <header class="text-center w-md-50 mx-auto mb-8">
-              <h2 class="h1 mb-0">Projects Committee</h2>
-            </header>
-
-            <p class="h5 mb-0">
-              Bring your passion in software engineering to the ICSSC Project committee and build technical skills by working on real development projects for the UCI community.
-              Experience different roles in software development such as front-end, back-end, full-stack, UI/UX design, etc.
-              Learn how to use version control and software development methodology while working in a real development team to prepare you for your career.
-            </p>
-
-
-            <br></br>
-            <div class="container">
-              <h3>Project Committee Members</h3>
-              <div class="row justify-content-center">
-                <div class="col-lg-3 mb-5 mb-lg-0">
-                  <p>Aaron Chen</p>
-                  <p>Ayush Satyavarpu</p>
-                  <p>Edward Wu</p>
-                  <p>Ileen Fan</p>
-                  <p>Pranav Reddy</p>
-                  <p>Sakshi Patil</p>
-                </div>
-
-                <div class="col-lg-3 mb-5 mb-lg-0">
-                  <p>Adarsh Pachori</p>
-                  <p>Chloe Luong</p>
-                  <p>Eric Pedley</p>
-                  <p>Johnson Dinh</p>
-                  <p>Randy Huynh</p>
-                  <p>Xuan Vu</p>
-                </div>
-
-                <div class="col-lg-3 mb-5 mb-lg-0">
-                  <p>Advay Anand</p>
-                  <p>Daniel Binoy</p>
-                  <p>Ethan Wong</p>
-                  <p>Kartik Bhagatwala</p>
-                  <p>Ryan Mohta</p>
-                  <p>Yasper De Jong</p>
-                </div>
-                <div class="col-lg-3 mb-5 mb-lg-0">
-                  <p>Ashlyn Riambon</p>
-                  <p>Douglas Hong</p>
-                  <p>Frederick Dong</p>
-                  <p>Kyle Pan</p>
-                  <p>Sahil Jagad</p>
-                </div>
-
-
-
-              </div>
-
-            </div>
-          </div>
-        </section>
-
-        <section class="text-center u-content-space">
-          <div class="container">
-            <header class="text-center w-md-50 mx-auto mb-8">
-              <h2 class="h1">Corporate Outreach Committee</h2>
-            </header>
-
-            <p class="h5 mb-0">
-              The Corporate Outreach Committee for ICSSC is responsible for establishing and maintaining professional and financial relationships with corporate supporters and alumni in the computer science industry.
-              These relationships are crucial for receiving financial sponsorship for ICS events, for hosting informational workshops held for UCI students, and for student recruiting purposes.
-              Our job is to network with alumni and company representatives to remind them that the School of ICS has national prestige, and to help them find something at UCI that appeals to their outreach and funding interests.
-            </p>
-
-
-            <br></br>
-            <div class="container">
-              <h3>Corporate Outreach Committee Members</h3>
-              <div class="row justify-content-center">
-                <div class="col-lg-3 mb-5 mb-lg-0">
-                  <p>Anthony Cho Cusimano</p>
-                </div>
-
-                <div class="col-lg-3 mb-5 mb-lg-0">
-                  <p>Catherine Zheng</p>
-                </div>
-
-                <div class="col-lg-3 mb-5 mb-lg-0">
-                  <p>Kirby Raja Ammari</p>
-                </div>
-
-                <div class="col-lg-3 mb-5 mb-lg-0">
-                  <p>Lawrence Lu</p>
-                </div>
-              </div>
-            </div>
-
-          </div>
-        </section>
-        <section class="text-center bg-light u-content-space">
-          <div class="container">
-            <header class="text-center w-md-50 mx-auto mb-8">
-              <h2 class="h1 mb-0">Alumni and Academic Affairs Committee</h2>
-            </header>
-
-            <p class="h5 mb-0">
-              Alumni and Academic Affairs Committee's mission is to reach out and build relationships with UCI faculty,
-              address the concerns of the ICS student body and to further them into academic and administrative cohesion & collaboration with the ICS student body.
-              The committee will also be in charge of bridging the Student Council with the Bren School alumni.
-            </p>
-
-            <br></br>
-            <div class="container">
-              <h3>Alumni and Academic Affairs Committee Members</h3>
-              <div class="row justify-content-center">
-                <div class="col-lg-3 mb-5 mb-lg-0">
-                  <p>Becky Dinh</p>
-                  <p>Meenakshi Mynampati</p>
-                </div>
-                <div class="col-lg-3 mb-5 mb-lg-0">
-                  <p>Jake Aaron Leue</p>
-                  <p>Raymond Chou</p>
-                </div>
-                <div class="col-lg-3 mb-5 mb-lg-0">
-                  <p>Jeffrey Chen Huang</p>
-                  <p>Vivan Chih</p>
-                </div>
-                <div class="col-lg-3 mb-5 mb-lg-0">
-                  <p>Lily Mingyi He</p>
-                  <p>Yifei (Migo) Liu</p>
-                </div>
-
-
-              </div>
-
-            </div>
-          </div>
-        </section>
+        {CommitteesJSON.map(committee => <Committee {...committee}/> )}
       </main>
     </>
   )
