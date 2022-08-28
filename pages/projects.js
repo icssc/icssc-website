ce7ff377419ea0eae3972452b906a8183a8b75c6
Avent Chiu <chiual@uci.edu>
2022-08-28 14:00:45 -0700

    Updated projects page

--- pages/projects.js
+++ pages/projects.js
@@ -5,23 +5,19 @@ function Project(props) {
     const {img, name, desc, src} = props
     return (
         <Col lg={5} md={12} className="p-4">
-            <Card style={{ width: 'auto', height: '100%', borderRadius:"30px", boxShadow:"2px 4px 10px" }}>
-                <img
-                style={{borderTopLeftRadius:"30px", borderTopRightRadius:"30px"}}
-                height="auto"
-                width="auto"
-                src={img}/>
+            <Card style={{ width: 'auto', height: '100%', borderRadius:"30px", boxShadow:"2px 4px 10px", border:"0px" }}>
+                <Card.Img variant="top" src={img} style={{height: '36vh', objectFit:"contain", borderTopLeftRadius:"30px", borderTopRightRadius:"30px"}} />
                 <Card.Body>
                     <Card.Title>
                         <h2><a href={src} target="_blank">{name} ↗</a></h2>
                     </Card.Title>
+                    <Card.Subtitle></Card.Subtitle>
                     <Card.Text>
                         <h5>{desc}</h5>
                     </Card.Text>
                 </Card.Body>
             </Card>
         </Col>
-        
     );
 }
 
