8311db0f9e227a0b9283ed55faceeed7c9ebbdeb
Avent Chiu <65209258+sirAvent@users.noreply.github.com>
2022-08-29 00:05:00 -0700

    Rewrite resource page to Next.js (#41)
    * Rewrite resource page to Next.js
    
    * Added clickable link to resources

--- pages/resources.js
+++ pages/resources.js
@@ -1,96 +1,22 @@
+import ResourceData from '../assets/data/resources.json'
+import { Container } from "react-bootstrap";
+
+function Resource(props) {
+  const { title, desc, src } = props;
+  return (
+      <Container className="text-center mb-5 mt-5">
+        <h1>{title}</h1>
+        <h5>{desc} <br/> Link: <a href={src}>{src}</a></h5>
+      </Container>
+  )
+}
+
 export default function Resources() {
   return (
     <>
       <h1>Resources</h1>
-      <main role="main">
-        <section class="container u-content-space u-bg-arrow-wrapper">
-          <div class="text-center u-bg-arrow-bottom">
-            <p class="h5 mb-0">ICS Student Council provides you with these resources that can help you learn about ICS at UCI! </p>
-          </div>
-        </section>
-
-        <section class="text-center bg-light u-content-space">
-          <div class="container">
-            <header class="text-center w-md-50 mx-auto mb-8">
-              <h2 class="h1">Quarter System Guide</h2>
-            </header>
-
-            <p class="h5 mb-0">
-              Check out some tips and resources to get used to the quarter system!<br />
-
-              Link: <a href="https://tinyurl.com/m98bdesw">https://tinyurl.com/m98bdesw</a>
-            </p>
-            <br></br>
-          </div>
-        </section>
-        <section class="text-center u-content-space">
-          <div class="container">
-            <header class="text-center w-md-50 mx-auto mb-8">
-              <h2 class="h1">Python Basics</h2>
-            </header>
-
-            <p class="h5 mb-0">
-              New to python? Want to strengthen the basics? Look at this document to get a quick overview! <br />
-              Link: <a href="https://tinyurl.com/8esj43">https://tinyurl.com/8esj43</a>
-            </p>
-            <br></br>
-          </div>
-        </section>
-
-        <section class="text-center bg-light u-content-space">
-          <div class="container">
-            <header class="text-center w-md-50 mx-auto mb-8">
-              <h2 class="h1">Computer Science vs Software Engineering vs Informatics</h2>
-            </header>
-
-            <p class="h5 mb-0">
-              Ever wondered what the difference between these three majors really is? Well look no further! Here is a
-              short guide as to what the differeneces and similarities between each of these majors are and how they are used
-              in a job setting.<br />
-
-              Link: <a href="https://tinyurl.com/fdkhra68">https://tinyurl.com/fdkhra68</a>
-            </p>
-
-
-            <br></br>
-
-          </div>
-        </section>
-
-
-
-        <section class="text-center u-content-space">
-          <div class="container">
-            <header class="text-center w-md-50 mx-auto mb-8">
-              <h2 class="h1">ICS Change of Majors</h2>
-            </header>
-
-            <p class="h5 mb-0">
-              To learn more about how to change your major within the school of ICS access this resource!<br />
-              Link: <a href="https://tinyurl.com/2xanne43">https://tinyurl.com/2xanne43</a>
-            </p>
-
-            <br></br>
-
-          </div>
-        </section>
-        <section class="text-center bg-light u-content-space">
-          <div class="container">
-            <header class="text-center w-md-50 mx-auto mb-8">
-              <h2 class="h1">Distinguishing ICS Majors</h2>
-            </header>
-
-            <p class="h5 mb-0">
-              Learn about the differences between all the ICS majors here!
-              <br />
-              Link: <a href="https://tinyurl.com/jex2xtn3">https://tinyurl.com/jex2xtn3</a>
-            </p>
-
-            <br />
-
-          </div>
-        </section>
-      </main>
+      <h5 className="text-center">ICS Student Council provides you with these resources that can help you learn about ICS at UCI! </h5>
+      {ResourceData.map(info => <Resource {...info} />)}
     </>
   )
 }
