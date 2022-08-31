448bb9f66bd3f56b310dfaba57948d56f496ab5d
Avent Chiu <chiual@uci.edu>
2022-08-30 19:04:31 -0700

    Restyled index for barebones structure

--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,16 @@ import styles from '/styles/Index.module.css';
 
 function TitleBanner(props) {
   return (
-    <Container fluid>
-      <h4 className={`${styles.heroTitle}`}>An organization to improve ICS students professionally, socially, and academically.</h4>
-      <p className={`${styles.heroSubtitle}`}>The club for students, by students at the University of California, Irvine</p>
-      <button type="button" className={`buttonLight`}>Get Involved</button>
-    </Container>
+    <>
+      <h4 className={`${styles.heroTitle}`}>ICS Student Council </h4>
+      {/* <p className={`${styles.heroSubtitle}`}>The club for students, by students at the University of California, Irvine</p> */}
+      <button type="button" className={`buttonLight ${styles.heroButton}`}>Get Involved</button>
+    </>
   )
 }
 
 function SponsorLogo(props) {
   const { id, logo, name } = props;
-
   return (
     <Col>
       <a href={`sponsors.html#${id}`}>
@@ -34,11 +33,11 @@ function SponsorLogo(props) {
 function Event(props) {
   const {src, title} = props;
   return (
-    <Col md={4} sm={12}>
+    <Col lg={6}>
       <img
       src={src}
-      width="90%"
-      style={{marginTop:"10px",marginBottom:"10px"}}
+      width="auto"
+      style={{margin:"20px"}}
       className={`${styles.imgRound}`}
       />
     </Col>
@@ -49,59 +48,57 @@ export default function Home() {
   return (
     <>
       <div className={`${styles.heroBanner}`}>
-        <TitleBanner />
+        <TitleBanner/>
       </div>
-      
-      <Container fluid>
-        <Row className="justify-content-center align-items-center m-2">
-          <Col lg={4} md={12} className="text-center m-2 ">
-            <img
-              className={`${styles.imgRound}`}
-              src="/assets/img/events/past/2021_fall/bonfire.jpeg"
-              width="100%"
-              alt="About Picture"
-            />
-          </Col>
-          <Col lg={1} md={0}></Col>
-          <Col lg={4} md={12} className="m-2">
-            <h2 className={`${styles.sectionTitle}`}>About Us</h2>
-              <p>
-                ICS Student Council was founded by students to better serve the ICS student body. We work to improve the lives of the students professionally, socially, and academically and deepen the connections students have with the Donald Bren School of Information and Computer Sciences and UCI Alumni.
-              </p>
-            <p>
-              We host social and technical events for all students and oversee several large open-source projects for UCI Students
-            </p>
+      <div className={`${styles.section}`}>
+      {/* Image Sticker or more pizazz <img 
+        src='https://outline-prod.imgix.net/20170316-PuyyaZXOIGnZf1201YOM?auto=format&q=60&w=1280&s=1191ab9b0ada186b5d18215ca9dcf7b4'
+        height='300px'
+        className={`${styles.coolImage}`}
+        /> */}
+      <Row className="text-center justify-content-center align-items-center m-2">
+        <Col lg={12} sm={12}>
+          <h2 className={`${styles.sectionTitle}`}>About Us</h2>
+          <p className={`${styles.sectionDesc}`}>
+            ICS Student Council, founded in 2010, is a student-run organization that serves ICS students. Our mission is to deepen students' connections with the Donald Bren School of Information and Computer Sciences & UCI Alumni and improve them professionally, socially, and academically.
+          </p>
+          <p className={`${styles.sectionDesc}`}><a href="/get-involved">Learn More →</a></p>
+        </Col>
+      </Row>
+      </div>
+
+      <div className={`${styles.sectionColorLight} `}>
+        <Row className="justify-content-center align-items-center text-center m-2">
+          <Col lg={12} sm={12}>
+            <h2 className={`${styles.sectionTitle}`}>Our Events</h2>
+            <p className={`${styles.sectionDesc}`}>We aim to host events that will better ICS Students academically, professionally, and socially.</p>
+            <p className={`${styles.sectionDesc}`}><a href="/events">View Events →</a></p>
           </Col>
         </Row>
+      </div>
 
-        <div style={{marginTop:"5%",paddingTop:"2.5%",paddingBottom:"5%"}}>
-          <Row className="justify-content-center align-items-center text-center m-2">
-            <Col lg={10} sm={12} className="m-2">
-              <h2 className={`${styles.sectionTitle}`} style={{marginBottom:""}}>Events</h2>
-                <Row>
-                  {EventsData["past"].slice(0,3).map(past => <Event {...past}/>)}
-                </Row>
-                <button type="button" className="buttonLight">View All</button>
+      <div className={`${styles.section}`}>
+        <Row className="justify-content-center align-items-center text-center m-2">
+            <Col lg={12} sm={12} className="m-2">
+            <h2 className={`${styles.sectionTitle}`}>Projects</h2>
+            <p className={`${styles.sectionDesc}`}>
+              Our open source projects are to help students succeed at finding, planning, executing, and managing technology projects. It is our goal to teach students useful technical knowledge outside of class. All students are welcome to contribute.
+            </p>
+            <p className={`${styles.sectionDesc}`}><a href="/projects">Learn More →</a></p>
             </Col>
-          </Row>
-        </div>
+        </Row>
+      </div>
 
-        <div className={`${styles.section}`} style={{marginTop:""}}>
-          <h2 className={`${styles.sectionTitle}`}>Projects</h2>
-          <p>
-            Section for projects
-          </p>
-        </div>
-        
+      <div className={`${styles.sectionColorLight}`}>
         <Row className="justify-content-center">
-        <h2 className={`${styles.sectionTitle}`}>Thanks to Our Sponsors!</h2>
+        <h2 className={`${styles.sectionTitle}`}>Thank You to Our Sponsors</h2>
           <Col sm={12} lg={10}>
             <Row>
               {Sponsors.map(sponsor => <SponsorLogo {...sponsor} key={sponsor.id}/>)}
             </Row>
           </Col>
         </Row>
-      </Container>
+      </div>
     </>
   )
 }
