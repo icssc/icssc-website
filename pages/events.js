5584a20ce79bef390d6c195fe97dbf327e9b911e
Avent Chiu <chiu7a@gmail.com>
2022-09-30 20:52:15 -0700

    Update Events page to display event cards

--- pages/events.js
+++ pages/events.js
@@ -1,35 +1,62 @@
 import Image from "next/image";
-import { Col, Container, Row, Button } from "react-bootstrap";
+import { Col, Container, Row, Button, Modal } from "react-bootstrap";
 import EventsData from '../assets/data/events.json';
+import React, { useState } from 'react';
 
-function Event(props) {
+import styles from '/styles/Events.module.scss';
+
+function EventModal(props) {
   const {src, title, time, desc, isCurrent, rsvp_src} = props;
   return (
-    <Container className="pt-5 pb-5 mt-5 mb-5">
-      <Row className="align-items-center">
-        <Col lg={6}>
-          <Image
-            className="rounded"
-            objectFit={"contain"}
-            src={src}
-            alt={`${title} image`}
-            height={400}
-            width={400}
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {title}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>{time}</p>
+        {desc}
+      </Modal.Body>
+      <Modal.Footer>
+        {isCurrent && <Button variant="success" href={rsvp_src}>RSVP</Button>}
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+
+function Event(props) {
+  const [modalShow, setModalShow] = useState(false);
+  const {src, title, time, desc, isCurrent, rsvp_src} = props;
+  return (
+    <>
+      <div className={`${styles.eventBox}`} style={{margin:"10px"}} onClick={() => setModalShow(true)}>
+          <img
+              src={src}
           />
-        </Col>
-        <Col lg={6}>
-          <Container>
-            <h1 className="mb-4">{title}</h1>
-            <h5 className="mb-4">{time}</h5>
-            <p>{desc}</p>
-            {isCurrent && 
-                <Button size="lg" href={rsvp_src}>RSVP</Button>
-            }
-          </Container>
-        </Col>
-      </Row>
-    </Container>
-  )
+          <h4 style={{marginTop:"10px", fontWeight:"600"}}>
+              {title}
+          </h4>
+          <p>{time}</p>
+      </div>
+      <EventModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        title={title}
+        desc={desc}
+        isCurrent={isCurrent}
+        time={time}
+        rsvp_src={rsvp_src}
+      />
+    </>
+  );
 }
 
 export default function Events() {
@@ -38,10 +65,23 @@ export default function Events() {
       <h1>Events</h1>
       <h1 className="text-center">Current Events</h1>
       {<p className="text-center">No current events! Stay tuned for more :)</p>}
-      {/* EventsData["current"].map(current => <Event {...current} isCurrent={true}/>) */}
+      <Row style={{justifyContent:"center"}}>
+      <div className={`${styles.sectionEvents} `}>
+        <div className={`${styles.eventsGrid}`}>
+            {EventsData["current"].map(past => <Event {...past} isCurrent={true}/>)}
+        </div>
+      </div>
+      </Row>
 
       <h1 className="text-center mt-5 pt-5">Past Events</h1>
-      {EventsData["past"].map(past => <Event {...past} isCurrent={false}/>)}
+      <Row style={{justifyContent:"center"}}>
+      <div className={`${styles.sectionEvents} `}>
+        <div className={`${styles.eventsGrid}`}>
+            {EventsData["past"].map(past => <Event {...past} isCurrent={false}/>)}
+        </div>
+      </div>
+      </Row>
+      
     </>
   )
 }
