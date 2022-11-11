b2d232ebf7d7d8fd0cf1c8b4c79e2c6503aa582a
Avent Chiu <chiu7a@gmail.com>
2022-11-10 20:32:02 -0800

    Moved logo svg to image folders and fixed crossOrigin syntax

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossOrigin="anonymous" />
         <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=DM+Sans" />
         <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Bungee" />
       </Head>
