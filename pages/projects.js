29f011ffbd703351a4d630edb6c166c26df2be65
Avent Chiu <chiu7a@gmail.com>
2023-01-14 04:05:37 -0800

    update ui

--- pages/projects.js
+++ pages/projects.js
@@ -1,33 +1,45 @@
 import ProjectData from '../assets/data/projects.json';
 import { Row, Col, Container, Card } from 'react-bootstrap';
-import styles from '/styles/Projects.module.css';
+import styles from '/styles/Projects.module.scss';
 
 function Project(props) {
-    const {img, name, desc, src} = props
+    const {img, name, desc, src, repo} = props
     return (
-        <div className={`${styles.projectBox}`} style={{margin:"10px"}}>
+        <div className={`${styles.projectGrid}`}>
             <img
-                src={'https://th.bing.com/th/id/OIP.gPjZVOnX-A2jyUrN3gcTcQHaFj?pid=ImgDet&rs=1'}
+                src={img}
             />
-            <h4 style={{marginTop:"10px", fontWeight:"600"}}>
-                <a target="_blank" href={src}>{name} ↗</a>
-            </h4>
-            <p>{desc}</p>
+            <div>
+                <h4>{name}</h4>
+                <p>{desc}</p>
+                <a href={src} rel="noreferrer" target="_blank">
+                    <button type='button'>View Project</button>
+                </a>
+                <a href={repo} rel="noreferrer" target="_blank">
+                    <button type='button'>View Repo</button>
+                </a>
+            </div>
         </div>
     );
 }
 
 export default function Projects() {
     return (
-        <>
-            <br></br>
-            <h4 className="text-center">Anyone is welcome to contribute to our projects! Checkout our <a href="https://github.com/icssc">GitHub to start contributing!</a></h4>
+        <Container>
+            <div className={styles.banner}>
             <h1>Projects</h1>
-            <div className={`${styles.sectionProjects} `}>
-                <div className={`${styles.projectGrid}`}>
+            
+            <p>ICSSC offers development projects to benefit the UCI community. 
+                Anyone is welcome to contribute to the projects at our <u><a href="https://github.com/icssc">GitHub</a></u>
+            </p>
+            
+            </div>
+            
+            <div>
+                <div>
                     {ProjectData.map((project, index) => <Project key={index} {...project}/>)}
                 </div>
             </div>
-        </>
+        </Container>
     );
 }
