9356b8c7961cae21ee05a410b747de17c101d862
Avent Chiu <chiual@uci.edu>
2023-01-15 18:40:11 -0800

    Update click box for contact link

--- pages/contacts.js
+++ pages/contacts.js
@@ -9,26 +9,34 @@ export default function Contacts() {
     <Container>
         <h1 className={styles.title}>Contact Us</h1>
         <div className={styles.contactContainer}>
-          <div className={styles.contactCard}>
-            <FaFacebook size={50} />
-            <h3 className="h5">Facebook</h3>
-            <p className=""><a href="https://www.facebook.com/ICSStudentCouncil/">ICSStudentCouncil</a></p>
-          </div>
-          <div className={styles.contactCard}>
-            <FaInstagram size={50} />
-            <h3 className="h5">Instagram</h3>
-            <p className=""><a href= "https://www.instagram.com/icssc.uci/">@icssc.uci</a></p>
-          </div>
-          <div className={styles.contactCard}>
-            <FaDiscord size={50} />
-            <h3 className="h5">Discord</h3>
-            <p className=""><a href="https://discord.gg/c4t5dGcM9S">ICSSC</a></p>
-          </div>
-          <div className={styles.contactCard}>
-            <FaEnvelope size={50} />
-            <h3 className="h5">Email</h3>
-            <p className=""><a href="mailto:icssc@uci.edu">icssc@uci.edu</a></p>
-          </div>
+          <a href="https://www.facebook.com/ICSStudentCouncil/">
+            <div className={styles.contactCard}>
+              <FaFacebook size={50} />
+              <h3 className="h5">Facebook</h3>
+              <p>ICSStudentCouncil</p>
+            </div>
+          </a>
+          <a href= "https://www.instagram.com/icssc.uci/">
+            <div className={styles.contactCard}>
+              <FaInstagram size={50} />
+              <h3 className="h5">Instagram</h3>
+              <p>@icssc.uci</p>
+            </div>
+          </a>
+          <a href="https://discord.gg/c4t5dGcM9S">
+            <div className={styles.contactCard}>
+              <FaDiscord size={50} />
+              <h3 className="h5">Discord</h3>
+              <p>ICSSC</p>
+            </div>
+          </a>
+          <a href="mailto:icssc@uci.edu">
+            <div className={styles.contactCard}>
+              <FaEnvelope size={50} />
+              <h3 className="h5">Email</h3>
+              <p>icssc@uci.edu</p>
+            </div>
+          </a>
         </div>
     </Container>
   )
