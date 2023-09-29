b76b09787778e977560d56b3aab3d43bbc2cd6e8
Kevin Wu <kevinwu098@gmail.com>
2023-09-29 00:26:54 -0700

    Refactor events

--- pages/events.js
+++ pages/events.js
@@ -1,11 +1,12 @@
 import { Row, Button, Modal } from "react-bootstrap";
-import EventsData from '../assets/data/events.json';
-import React, { useState } from 'react';
+import EventsData from "../assets/data/events.json";
+import React, { useState } from "react";
+import Image from "next/image";
 
-import styles from '/styles/Events.module.scss';
+import styles from "/styles/Events.module.scss";
 
 function EventModal(props) {
-  const {src, title, time, location, desc, iscurrent, rsvp_src} = props;
+  const { title, time, location, desc, rsvp_src } = props;
   return (
     <Modal
       {...props}
@@ -14,37 +15,37 @@ function EventModal(props) {
       centered
     >
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          {title}
-        </Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">{title}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <p>{time} | {location}</p>
+        <p>
+          {time} | {location}
+        </p>
         {desc}
       </Modal.Body>
       <Modal.Footer>
-        {rsvp_src && <Button variant="success" href={rsvp_src}>RSVP</Button>}
+        {rsvp_src && (
+          <Button variant="success" href={rsvp_src}>
+            RSVP
+          </Button>
+        )}
         <Button onClick={props.onHide}>Close</Button>
       </Modal.Footer>
     </Modal>
   );
 }
 
-
 function Event(props) {
   const [modalShow, setModalShow] = useState(false);
-  const {src, title, time, location, desc, iscurrent, rsvp_src} = props;
+  const { src, title, time, location, desc, iscurrent, rsvp_src } = props;
   return (
     <>
       <div className={`${styles.eventBox}`} onClick={() => setModalShow(true)}>
-          <img
-              src={src}
-          />
-          <h4 style={{marginTop:"10px", fontWeight:"600"}}>
-              {title}
-          </h4>
-          <p>{location}<br/> {time}</p>
-          
+        <Image src={src} alt="Event Image" width={100} height={100} />
+        <h4 style={{ marginTop: "10px", fontWeight: "600" }}>{title}</h4>
+        <p>
+          {location} {time}
+        </p>
       </div>
       <EventModal
         show={modalShow}
@@ -63,33 +64,39 @@ function Event(props) {
 export default function Events() {
   return (
     <>
-      {/* Event Title Section */}
       <div className="sectionAlt">
-      <h2>Upcoming Events</h2>
-      {<p>No current events! Stay tuned for more :)</p>}
-      {/* Uncomment This To Display Current Events */}
-      <Row style={{justifyContent:"center"}}>
-        <div className={`${styles.sectionEvents} `}>
-          <div className={`${styles.eventsGrid}`}>
-              {EventsData["current"].map(past => <Event {...past} iscurrent="true" key={past.title}/>)}
-          </div>
-        </div>
-      </Row>
+        {/* Event Title Section */}
+        <h2>Upcoming Events</h2>
+
+        {/* Events */}
+        {EventsData.current.length > 0 ? (
+          <Row style={{ justifyContent: "center" }}>
+            <div className={`${styles.sectionEvents} `}>
+              <div className={`${styles.eventsGrid}`}>
+                {EventsData["current"].map((event) => (
+                  <Event {...event} iscurrent="true" key={event.title} />
+                ))}
+              </div>
+            </div>
+          </Row>
+        ) : (
+          <p>No current events! Stay tuned for more {":)"}</p>
+        )}
       </div>
 
-      
       {/* Event Past Section */}
       <div className="section">
         <h2>Past Events</h2>
-        <Row style={{justifyContent:"center"}}>
-        <div className={`${styles.sectionEvents} `}>
-          <div className={`${styles.eventsGrid}`}>
-              {EventsData["past"].map(past => <Event {...past} isCurrent={false} key={past.title}/>)}
+        <Row style={{ justifyContent: "center" }}>
+          <div className={`${styles.sectionEvents} `}>
+            <div className={`${styles.eventsGrid}`}>
+              {EventsData.past.map((pastEvent) => (
+                <Event {...pastEvent} isCurrent={false} key={pastEvent.title} />
+              ))}
+            </div>
           </div>
-        </div>
         </Row>
       </div>
-      
     </>
-  )
+  );
 }
