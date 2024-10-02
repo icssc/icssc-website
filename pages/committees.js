e9fa106e16ee2d2eb35ba2fc82e6d69bcb6539ab
Kevin Wu <kevinwu098@gmail.com>
2024-10-02 00:19:43 -0700

    chore: update committees to say 'six'

--- pages/committees.js
+++ pages/committees.js
@@ -1,34 +1,38 @@
-import CommitteesJSON from '../assets/data/committees.json';
-import { Container } from 'react-bootstrap'
-import styles from '/styles/Committees.module.scss'
+import CommitteesJSON from "../assets/data/committees.json";
+import { Container } from "react-bootstrap";
+import styles from "/styles/Committees.module.scss";
 
 function Committee(props) {
-  const {name, desc, graphic, members} = props;
+  const { name, desc, graphic, members } = props;
   return (
     <div className={styles.committee}>
-      <img
-        src={graphic}
-        alt={name}
-      />
+      <img src={graphic} alt={name} />
       <h2>{name}</h2>
       <p>{desc}</p>
       <>
         <div>
-          {members.map(member => <p key={member}>{member}</p>)}
+          {members.map((member) => (
+            <p key={member}>{member}</p>
+          ))}
         </div>
       </>
     </div>
-  )
+  );
 }
 
 export default function Committees() {
   return (
     <Container>
       <div className={styles.banner}>
-      <h1>Committees</h1>
-        <p>ICS Student Council has five committees to provide different ways for students to contribute to the community. </p>
+        <h1>Committees</h1>
+        <p>
+          ICS Student Council has six committees to provide different ways for
+          students to contribute to the community.{" "}
+        </p>
       </div>
-      {CommitteesJSON.map(committee => <Committee {...committee} key={committee.name}/> )}
+      {CommitteesJSON.map((committee) => (
+        <Committee {...committee} key={committee.name} />
+      ))}
     </Container>
-  )
+  );
 }
